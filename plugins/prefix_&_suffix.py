80fd1cd47d845422ea2f7a68959242e0463721a1
Noob Dev <kingceylegend@gmail.com>
2025-04-11 22:56:01 +0100

    Update prefix_&_suffix.py

--- plugins/prefix_&_suffix.py
+++ plugins/prefix_&_suffix.py
@@ -37,15 +37,15 @@ from helper.database import digital_botz
 @Client.on_message(filters.private & filters.command('set_prefix'))
 async def add_prefix(client, message):
     if len(message.command) == 1:
-        return await message.reply_text("**__Give The Prefix__\n\nExᴀᴍᴩʟᴇ:- `/set_prefix @Rkn_Bots`**")
+        return await message.reply_text("**__Donnez le préfixe__\n\nExemple:- `/set_prefix @BotZFlix`**")
     prefix = message.text.split(" ", 1)[1]
-    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    RknDev = await message.reply_text("Veuillez patienter ...", reply_to_message_id=message.id)
     await digital_botz.set_prefix(message.from_user.id, prefix)
     await RknDev.edit("__**✅ ᴘʀᴇꜰɪx ꜱᴀᴠᴇᴅ**__")
 
 @Client.on_message(filters.private & filters.command('del_prefix'))
 async def delete_prefix(client, message):
-    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    RknDev = await message.reply_text("Veuillez patienter ...", reply_to_message_id=message.id)
     prefix = await digital_botz.get_prefix(message.from_user.id)
     if not prefix:
         return await RknDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴘʀᴇꜰɪx**__")
@@ -54,7 +54,7 @@ async def delete_prefix(client, message):
 
 @Client.on_message(filters.private & filters.command('see_prefix'))
 async def see_prefix(client, message):
-    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    RknDev = await message.reply_text("Veuillez patienter ...", reply_to_message_id=message.id)
     prefix = await digital_botz.get_prefix(message.from_user.id)
     if prefix:
         await RknDev.edit(f"**ʏᴏᴜʀ ᴘʀᴇꜰɪx:-**\n\n`{prefix}`")
@@ -67,27 +67,27 @@ async def add_suffix(client, message):
     if len(message.command) == 1:
         return await message.reply_text("**__Give The Suffix__\n\nExᴀᴍᴩʟᴇ:- `/set_suffix @Rkn_Bots`**")
     suffix = message.text.split(" ", 1)[1]
-    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    RknDev = await message.reply_text("Veuillez patienter ...", reply_to_message_id=message.id)
     await digital_botz.set_suffix(message.from_user.id, suffix)
     await RknDev.edit("__**✅ ꜱᴜꜰꜰɪx ꜱᴀᴠᴇᴅ**__")
 
 @Client.on_message(filters.private & filters.command('del_suffix'))
 async def delete_suffix(client, message):
-    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    RknDev = await message.reply_text("Veuillez patienter ...", reply_to_message_id=message.id)
     suffix = await digital_botz.get_suffix(message.from_user.id)
     if not suffix:
-        return await RknDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ꜱᴜꜰꜰɪx**__")
+        return await RknDev.edit("__**😔 Vous n'avez pas de suffixe**__")
     await digital_botz.set_suffix(message.from_user.id, None)
-    await RknDev.edit("__**❌️ ꜱᴜꜰꜰɪx ᴅᴇʟᴇᴛᴇᴅ**__")
+    await RknDev.edit("__**❌️ Suffixe supprimé**__")
 
 @Client.on_message(filters.private & filters.command('see_suffix'))
 async def see_suffix(client, message):
-    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    RknDev = await message.reply_text("Veuillez patienter ...", reply_to_message_id=message.id)
     suffix = await digital_botz.get_suffix(message.from_user.id)
     if suffix:
-        await RknDev.edit(f"**ʏᴏᴜʀ ꜱᴜꜰꜰɪx:-**\n\n`{suffix}`")
+        await RknDev.edit(f"**Votre suffixe:-**\n\n`{suffix}`")
     else:
-        await RknDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ꜱᴜꜰꜰɪx**__")
+        await RknDev.edit("__**😔 Vous n'avez pas de suffixe.**__")
 
 # Rkn Developer 
 # Don't Remove Credit 😔
