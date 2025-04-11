f639e8d25e38ae53206ae21017e1b60328617047
Noob Dev <kingceylegend@gmail.com>
2025-04-11 22:31:20 +0100

    Update thumb_&_cap.py

--- plugins/thumb_&_cap.py
+++ plugins/thumb_&_cap.py
@@ -35,57 +35,57 @@ from helper.database import digital_botz
 
 @Client.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
-    rkn = await message.reply_text("__**ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ**__")
+    rkn = await message.reply_text("__**Veuillez patienter**__")
     if len(message.command) == 1:
-       return await rkn.edit("**__Gɪᴠᴇ Tʜᴇ Cᴀᴩᴛɪᴏɴ__\n\nExᴀᴍᴩʟᴇ:- `/set_caption {filename}\n\n💾 Sɪᴢᴇ: {filesize}\n\n⏰ Dᴜʀᴀᴛɪᴏɴ: {duration}`**")
+       return await rkn.edit("**__Donnez la légende__\n\nExemple:- `/set_caption {filename}\n\n💾 Poids: {filesize}\n\n⏰ Durée: {duration}`**")
     caption = message.text.split(" ", 1)[1]
     await digital_botz.set_caption(message.from_user.id, caption=caption)
-    await rkn.edit("__**✅ Cᴀᴩᴛɪᴏɴ Sᴀᴠᴇᴅ**__")
+    await rkn.edit("__**✅ Légende enregistrée**__")
    
 @Client.on_message(filters.private & filters.command(['del_caption', 'delete_caption', 'delcaption']))
 async def delete_caption(client, message):
-    rkn = await message.reply_text("__**ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ**__")
+    rkn = await message.reply_text("__**Veuillez patienter**__")
     caption = await digital_botz.get_caption(message.from_user.id)  
     if not caption:
-       return await rkn.edit("__**😔 Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Aɴy Cᴀᴩᴛɪᴏɴ**__")
+       return await rkn.edit("__**😔 Vous n'avez pas de légende**__")
     await digital_botz.set_caption(message.from_user.id, caption=None)
-    await rkn.edit("__**❌️ Cᴀᴩᴛɪᴏɴ Dᴇʟᴇᴛᴇᴅ**__")
+    await rkn.edit("__**❌️ Légende supprimée**__")
                                        
 @Client.on_message(filters.private & filters.command(['see_caption', 'view_caption']))
 async def see_caption(client, message):
-    rkn = await message.reply_text("__**ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ**__")
+    rkn = await message.reply_text("__**Veuillez patienter**__")
     caption = await digital_botz.get_caption(message.from_user.id)  
     if caption:
-       await rkn.edit(f"**Yᴏᴜ'ʀᴇ Cᴀᴩᴛɪᴏɴ:-**\n\n`{caption}`")
+       await rkn.edit(f"**Votre légende:-**\n\n`{caption}`")
     else:
-       await rkn.edit("__**😔 Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Aɴy Cᴀᴩᴛɪᴏɴ**__")
+       await rkn.edit("__**😔 Vous n'avez pas de légende.**__")
 
-@Client.on_message(filters.private & filters.command(['view_thumb', 'viewthumb']))
+@Client.on_message(filters.private & filters.command(['view_thVous n'avez pas de légende.umb', 'viewthumb']))
 async def viewthumb(client, message):
-    rkn = await message.reply_text("__**ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ**__")
+    rkn = await message.reply_text("__**Veuillez patienter**__")
     thumb = await digital_botz.get_thumbnail(message.from_user.id)
     if thumb:
         await client.send_photo(chat_id=message.chat.id, photo=thumb)
         await rkn.delete()
     else:
-        await rkn.edit("😔 __**Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Aɴy Tʜᴜᴍʙɴᴀɪʟ**__") 
+        await rkn.edit("😔 __**Vous n'avez pas de miniature**__") 
 		
 @Client.on_message(filters.private & filters.command(['del_thumb', 'delete_thumb', 'delthumb']))
 async def removethumb(client, message):
-    rkn = await message.reply_text("__**ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ**__")
+    rkn = await message.reply_text("__**Veuillez patienter**__")
     thumb = await digital_botz.get_thumbnail(message.from_user.id)
     if thumb:
         await digital_botz.set_thumbnail(message.from_user.id, file_id=None)
         await rkn.edit("❌️ __**Tʜᴜᴍʙɴᴀɪʟ Dᴇʟᴇᴛᴇᴅ**__")
         return
-    await rkn.edit("😔 __**Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Aɴy Tʜᴜᴍʙɴᴀɪʟ**__")
+    await rkn.edit("😔 __**Vous n'avez pas de miniature**__")
 
 
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client, message):
-    rkn = await message.reply_text("__**ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ**__")
+    rkn = await message.reply_text("__**Veuillez patienter**__")
     await digital_botz.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
-    await rkn.edit("✅️ __**Tʜᴜᴍʙɴᴀɪʟ Sᴀᴠᴇᴅ**__")
+    await rkn.edit("✅️ __**Miniature enregistrée**__")
 
 # (c) @RknDeveloperr
 # Rkn Developer 
