fcb7c248ade0dcb7b59178119fd7e079edcc8bda
Noob Dev <kingceylegend@gmail.com>
2025-04-12 06:40:46 +0100

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -76,17 +76,17 @@ async def rename_start(client, message):
         remain = int(limit) - int(used)
         used_percentage = int(used) / int(limit) * 100
         if remain < int(rkn_file.file_size):
-            return await message.reply_text(f"{used_percentage:.2f}% Of Daily Upload Limit {humanbytes(limit)}.\n\n Media Size: {filesize}\n Your Used Daily Limit {humanbytes(used)}\n\nYou have only **{humanbytes(remain)}** Data.\nPlease, Buy Premium Plan s.", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🪪 Uᴘɢʀᴀᴅᴇ", callback_data="plans")]]))
+            return await message.reply_text(f"{used_percentage:.2f}% De la limite de téléchargement quotidienne {humanbytes(limit)}.\n\n Taille du média : {filesize}\n Votre limite quotidienne utilisée {humanbytes(used)}\n\nVous n'avez que **{humanbytes(remain)}** Données.\nVeuillez acheter un plan premium.", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🪪 Mise à niveau", callback_data="plans")]]))
          
 	    
     if await digital_botz.has_premium_access(user_id) and client.premium:
         if not Config.STRING_SESSION:
             if rkn_file.file_size > 2000 * 1024 * 1024:
-                 return await message.reply_text("Sᴏʀʀy Bʀᴏ Tʜɪꜱ Bᴏᴛ Iꜱ Dᴏᴇꜱɴ'ᴛ Sᴜᴩᴩᴏʀᴛ Uᴩʟᴏᴀᴅɪɴɢ Fɪʟᴇꜱ Bɪɢɢᴇʀ Tʜᴀɴ 2Gʙ+")
+                 return await message.reply_text("Désolé frère, ce bot ne prend pas en charge le téléchargement de fichiers supérieurs à 2 Go.")
 
         try:
             await message.reply_text(
-            text=f"**__ᴍᴇᴅɪᴀ ɪɴꜰᴏ:\n\n◈ ᴏʟᴅ ꜰɪʟᴇ ɴᴀᴍᴇ: `{filename}`\n\n◈ ᴇxᴛᴇɴꜱɪᴏɴ: `{extension_type.upper()}`\n◈ ꜰɪʟᴇ ꜱɪᴢᴇ: `{filesize}`\n◈ ᴍɪᴍᴇ ᴛʏᴇᴩ: `{mime_type}`\n◈ ᴅᴄ ɪᴅ: `{dcid}`\n\nᴘʟᴇᴀsᴇ ᴇɴᴛᴇʀ ᴛʜᴇ ɴᴇᴡ ғɪʟᴇɴᴀᴍᴇ ᴡɪᴛʜ ᴇxᴛᴇɴsɪᴏɴ ᴀɴᴅ ʀᴇᴘʟʏ ᴛʜɪs ᴍᴇssᴀɢᴇ....__**",
+            text=f"**__Informations sur les médias:\n\n◈ Ancien nom de fichier: `{filename}`\n\n◈ Extension: `{extension_type.upper()}`\n◈ Taille du fichier: `{filesize}`\n◈ Type MIME: `{mime_type}`\n◈ ᴅᴄ ɪᴅ: `{dcid}`\n\nVeuillez entrer le nouveau nom de fichier avec l'extension et répondre à ce message.....__**",
 	    reply_to_message_id=message.id,  
 	    reply_markup=ForceReply(True)
         )       
@@ -94,7 +94,7 @@ async def rename_start(client, message):
         except FloodWait as e:
             await sleep(e.value)
             await message.reply_text(
-            text=f"**__ᴍᴇᴅɪᴀ ɪɴꜰᴏ:\n\n◈ ᴏʟᴅ ꜰɪʟᴇ ɴᴀᴍᴇ: `{filename}`\n\n◈ ᴇxᴛᴇɴꜱɪᴏɴ: `{extension_type.upper()}`\n◈ ꜰɪʟᴇ ꜱɪᴢᴇ: `{filesize}`\n◈ ᴍɪᴍᴇ ᴛʏᴇᴩ: `{mime_type}`\n◈ ᴅᴄ ɪᴅ: `{dcid}`\n\nᴘʟᴇᴀsᴇ ᴇɴᴛᴇʀ ᴛʜᴇ ɴᴇᴡ ғɪʟᴇɴᴀᴍᴇ ᴡɪᴛʜ ᴇxᴛᴇɴsɪᴏɴ ᴀɴᴅ ʀᴇᴘʟʏ ᴛʜɪs ᴍᴇssᴀɢᴇ....__**",
+            text=f"**__Informations sur les médias:\n\n◈ Ancien nom de fichier: `{filename}`\n\n◈ Extension: `{extension_type.upper()}`\n◈ Taille du fichier: `{filesize}`\n◈ Type MIME: `{mime_type}`\n◈ ᴅᴄ ɪᴅ: `{dcid}`\n\nVeuillez entrer le nouveau nom de fichier avec l'extension et répondre à ce message....__**",
 	    reply_to_message_id=message.id,  
 	    reply_markup=ForceReply(True)
         )
@@ -102,11 +102,11 @@ async def rename_start(client, message):
             pass
     else:
         if rkn_file.file_size > 2000 * 1024 * 1024 and client.premium:
-            return await message.reply_text("If you want to rename 4GB+ files then you will have to buy premium. /plans")
+            return await message.reply_text("Si vous souhaitez renommer des fichiers de plus de 4 Go, vous devrez acheter un plan premium. /plans")
 
         try:
             await message.reply_text(
-            text=f"**__ᴍᴇᴅɪᴀ ɪɴꜰᴏ:\n\n◈ ᴏʟᴅ ꜰɪʟᴇ ɴᴀᴍᴇ: `{filename}`\n\n◈ ᴇxᴛᴇɴꜱɪᴏɴ: `{extension_type.upper()}`\n◈ ꜰɪʟᴇ ꜱɪᴢᴇ: `{filesize}`\n◈ ᴍɪᴍᴇ ᴛʏᴇᴩ: `{mime_type}`\n◈ ᴅᴄ ɪᴅ: `{dcid}`\n\nᴘʟᴇᴀsᴇ ᴇɴᴛᴇʀ ᴛʜᴇ ɴᴇᴡ ғɪʟᴇɴᴀᴍᴇ ᴡɪᴛʜ ᴇxᴛᴇɴsɪᴏɴ ᴀɴᴅ ʀᴇᴘʟʏ ᴛʜɪs ᴍᴇssᴀɢᴇ....__**",
+            text=f"**__Informations sur les médias:\n\n◈ Ancien nom de fichier: `{filename}`\n\n◈ Extension: `{extension_type.upper()}`\n◈ Taille du fichier: `{filesize}`\n◈ Type MIME: `{mime_type}`\n◈ ᴅᴄ ɪᴅ: `{dcid}`\n\nVeuillez entrer le nouveau nom de fichier avec l'extension et répondre à ce message....__**",
 	    reply_to_message_id=message.id,  
 	    reply_markup=ForceReply(True)
         )       
@@ -144,7 +144,7 @@ async def refunc(client, message):
         elif file.media == MessageMediaType.AUDIO:
             button.append([InlineKeyboardButton("🎵 Aᴜᴅɪᴏ", callback_data = "upload_audio")])
         await message.reply(
-            text=f"**Sᴇʟᴇᴄᴛ Tʜᴇ Oᴜᴛᴩᴜᴛ Fɪʟᴇ Tyᴩᴇ**\n**• Fɪʟᴇ Nᴀᴍᴇ :-**`{new_name}`",
+            text=f"**Sélectionnez le type de fichier de sortie**\n**• Nom de fichier :-**`{new_name}`",
             reply_to_message_id=file.id,
             reply_markup=InlineKeyboardMarkup(button)
         )
@@ -153,7 +153,7 @@ async def refunc(client, message):
 
 @Client.on_callback_query(filters.regex("upload"))
 async def doc(bot, update):
-    rkn_processing = await update.message.edit("`Processing...`")
+    rkn_processing = await update.message.edit("`Traitement...`")
 	
     # Creating Directory for Metadata
     if not os.path.isdir("Metadata"):
@@ -170,7 +170,7 @@ async def doc(bot, update):
         suffix = await digital_botz.get_suffix(user_id)
         new_filename = add_prefix_suffix(new_filename_, prefix, suffix)
     except Exception as e:
-        return await rkn_processing.edit(f"⚠️ Something went wrong can't able to set Prefix or Suffix ☹️ \n\n❄️ Contact My Creator -> @RknDeveloperr\nError: {e}")
+        return await rkn_processing.edit(f"⚠️ Une erreur s'est produite, impossible de définir le préfixe ou le suffixe ☹️ \n\n❄️ Contactez mon créateur -> @Kingcey\nErreur: {e}")
 
     # msg file location 
     file = update.message.reply_to_message
@@ -181,7 +181,7 @@ async def doc(bot, update):
     
     metadata_path = f"Metadata/{new_filename}"    
 
-    await rkn_processing.edit("`Try To Download....`")
+    await rkn_processing.edit("`Essayez de télécharger....`")
     if bot.premium and bot.uploadlimit:
         limit = user_data.get('uploadlimit', 0)
         used = user_data.get('used_limit', 0)
@@ -201,13 +201,13 @@ async def doc(bot, update):
     if (metadata_mode):        
         metadata = await digital_botz.get_metadata_code(user_id)
         if metadata:
-            await rkn_processing.edit("I Fᴏᴜɴᴅ Yᴏᴜʀ Mᴇᴛᴀᴅᴀᴛᴀ\n\n__**Pʟᴇᴀsᴇ Wᴀɪᴛ...**__\n**Aᴅᴅɪɴɢ Mᴇᴛᴀᴅᴀᴛᴀ Tᴏ Fɪʟᴇ....**")            
+            await rkn_processing.edit("J'ai trouvé vos métadonnées\n\n__**Veuillez patienter...**__\n**Ajout des métadonnées au fichier....**")            
             if change_metadata(dl_path, metadata_path, metadata):            
                 await rkn_processing.edit("Metadata Added.....")
                 print("Metadata Added.....")
-        await rkn_processing.edit("**Metadata added to the file successfully ✅**\n\n**Tʀyɪɴɢ Tᴏ Uᴩʟᴏᴀᴅɪɴɢ....**")
+        await rkn_processing.edit("**Métadonnées ajoutées au fichier avec succès ✅**\n\n**Essai de téléchargement....**")
     else:
-        await rkn_processing.edit("`Try To Uploading....`")
+        await rkn_processing.edit("`Essayer de télécharger....`")
 	    
     duration = 0
     try:
@@ -231,7 +231,7 @@ async def doc(bot, update):
              if bot.premium and bot.uploadlimit:
                  used_remove = int(used) - int(media.file_size)
                  await digital_botz.set_used_limit(user_id, used_remove)
-             return await rkn_processing.edit(text=f"Yᴏᴜʀ Cᴀᴩᴛɪᴏɴ Eʀʀᴏʀ Exᴄᴇᴩᴛ Kᴇyᴡᴏʀᴅ Aʀɢᴜᴍᴇɴᴛ ●> ({e})")             
+             return await rkn_processing.edit(text=f"Erreur de légende, sauf l'argument de mot-clé. ●> ({e})")             
     else:
          caption = f"**{new_filename}**"
  
@@ -337,7 +337,7 @@ async def doc(bot, update):
 # please give credit 🙏🥲
 		    
     await remove_path(ph_path, file_path, dl_path, metadata_path)
-    return await rkn_processing.edit("Uploaded Successfully....")
+    return await rkn_processing.edit("Téléchargé avec succès.\n\nSi vous vouliez un bot pareil, contactez mon créateur: @Kingcey....")
     
 #@RknDeveloper
 #✅ Team-RknDeveloper
