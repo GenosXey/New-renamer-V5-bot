1701e46479ad763983dcc17f777ba5b65aa99515
Noob Dev <kingceylegend@gmail.com>
2025-04-13 10:05:58 +0100

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -43,20 +43,18 @@ from helper.utils import humanbytes
 from plugins import __version__ as _bot_version_, __developer__, __database__, __library__, __language__, __programer__
 
 upgrade_button = InlineKeyboardMarkup([[        
-        InlineKeyboardButton('Acheter Premium ✓', user_id=int(7428552084)),
+        InlineKeyboardButton('Acheter Premium ✓', user_id=int(6705898491)),
          ],[
         InlineKeyboardButton("Retour", callback_data = "start")
 ]])
 
 upgrade_trial_button = InlineKeyboardMarkup([[        
-        InlineKeyboardButton('Acheter Premium ✓', user_id=int(7428552084)),
+        InlineKeyboardButton('Acheter Premium ✓', user_id=int(6705898491)),
          ],[
         InlineKeyboardButton("Essai - 12 heures ✓", callback_data = "give_trial"),
         InlineKeyboardButton("Retour", callback_data = "start")
 ]])
 
-
-        
 @Client.on_message(filters.private & filters.command("start"))
 async def start(client, message):
     start_button = [[        
@@ -70,61 +68,33 @@ async def start(client, message):
     if client.premium:
         start_button.append([InlineKeyboardButton('💸 Mettre à niveau vers premium 💸', callback_data='upgrade')])
 
-    # ✅ Send sticker before start message added by me: (@Kingcey)
-    await message.reply_sticker("CAACAgIAAxkBAAIE8mf7IT_aZz3nBYBnDZvgd3qI7OCpAALMAQACPQ3oBMBrXcbV0L2fHgQ")
+    # ✅ Affiche tous les stickers l’un après l’autre puis les supprime
+    stickers = [
+        "CAACAgUAAxkBAAIE4Gf6qY1590UCET8-25D_Mi5OsSduAAImCQACzoAZVchVZroLpXfgHgQ",
+        "CAACAgUAAxkBAAIFOGf7e3t7xbexqML4lUBov33y0fwWAALgCAACW_HQVoT7skQi0IBNHgQ",
+        "CAACAgUAAxkBAAIFPGf7e32N9GQtxFUBhRpnBcdq-n2VAAIUCwACrZTRViZ7bFXnR26eHgQ",
+        "CAACAgUAAxkBAAIFQGf7e4BWBoddo5yVau4uEnW05RtbAAKLCwACwurRVj5eDbteWRIYHgQ",
+        "CAACAgUAAxkBAAIFRGf7e4JmicaVg9hMejExAlZj778PAAITCAACVknZVofykXMFYuv7HgQ"
+    ]
+
+    for sticker_id in stickers:
+        sent = await message.reply_sticker(sticker_id)
+        await asyncio.sleep(1.5)
+        await sent.delete()
 
     user = message.from_user
     await digital_botz.add_user(client, message) 
     if Config.RKN_PIC:
-        await message.reply_photo(Config.RKN_PIC, caption=rkn.START_TXT.format(user.mention), reply_markup=InlineKeyboardMarkup(start_button))    
+        await message.reply_photo(
+            Config.RKN_PIC,
+            caption=rkn.START_TXT.format(user.mention),
+            reply_markup=InlineKeyboardMarkup(start_button),
+            parse_mode="HTML"
+        )
     else:
-        await message.reply_text(text=rkn.START_TXT.format(user.mention), reply_markup=InlineKeyboardMarkup(start_button), disable_web_page_preview=True)
-
-
-@Client.on_message(filters.private & filters.command("myplan"))
-async def myplan(client, message):
-    if not client.premium:
-        return # premium mode disabled ✓
-
-    user_id = message.from_user.id
-    user = message.from_user.mention
-
-    if await digital_botz.has_premium_access(user_id):
-        data = await digital_botz.get_user(user_id)
-        expiry_str_in_ist = data.get("expiry_time")
-        time_left_str = expiry_str_in_ist - datetime.datetime.now()
-
-        text = f"ᴜˢᴇʀ :- {user}\nᴜˢᴇʀ ɪᴅ :- <code>{user_id}</code>\n"
-
-        if client.uploadlimit:
-            await digital_botz.reset_uploadlimit_access(user_id)                
-            user_data = await digital_botz.get_user_data(user_id)
-            limit = user_data.get('uploadlimit', 0)
-            used = user_data.get('used_limit', 0)
-            remain = int(limit) - int(used)
-            type = user_data.get('usertype', "Free")
-
-            text += f"ᴘʟᴀɴ :- `{type}`\nᴅᴀɪʟʏ ᴜᴹʟᴏᴀᴅ ʟɪᴛ :- `{humanbytes(limit)}`\nᴝʏᴍʏ ᴜʀᴇᴅ :- `{humanbytes(used)}`\nʁᴇᴛᴀɪɴ :- `{humanbytes(remain)}`\n"
-
-        text += f"ᴛɪᴍᴇ ʟᴇɿᴛ : {time_left_str}\nᴇᴀᴘɪʀʏ ᴅᴀᴛᴇ : {expiry_str_in_ist}"
-
-        await message.reply_text(text, quote=True)
-
-    else:
-        if client.uploadlimit:
-            user_data = await digital_botz.get_user_data(user_id)
-            limit = user_data.get('uploadlimit', 0)
-            used = user_data.get('used_limit', 0)
-            remain = int(limit) - int(used)
-            type = user_data.get('usertype', "Free")
-
-            text = f"ᴜˢᴇʀ :- {user}\nᴜˢᴇʀ ɪᴅ :- <code>{user_id}</code>\nᴘʟᴀɴ :- `{type}`\nᴅᴀɪʟʏ ᴜᴹʟᴏᴀᴅ ʟɪᴛ :- `{humanbytes(limit)}`\nᴝʏᴍʏ ᴜʀᴇᴅ :- `{humanbytes(used)}`\nʁᴇᴛᴀɪɴ :- `{humanbytes(remain)}`\nᴇᴘɪʀᴇᴄ ᴅᴀᴛᴇ :- ʟɪᴟᴟᴇᴝᴇ"
-
-            await message.reply_text(text, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("💸 Vérifier les plans premium 💸", callback_data='upgrade')]]), quote=True)
-
-        else:
-            m=await message.reply_sticker("CAACAgIAAxkBAAIE8mf7IT_aZz3nBYBnDZvgd3qI7OCpAALMAQACPQ3oBMBrXcbV0L2fHgQ")
-            await message.reply_text(f"Hey {user},\n\nVous n'avez pas de plans premium actifs. Si vous souhaitez souscrire à un plan premium, contactez @Kingcey 👇",
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("💸 Kingcey Premium 💸", url='t.me/kingcey')]]))			 
-            await asyncio.sleep(2)
-            await m.delete()
+        await message.reply_text(
+            text=rkn.START_TXT.format(user.mention),
+            reply_markup=InlineKeyboardMarkup(start_button),
+            disable_web_page_preview=True,
+            parse_mode="HTML"
+        )
